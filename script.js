8ef6fbe4b32c5de85c479859ff30ba73f021abca
GauravK23 <gauravkumar233@lpu.in>
2023-10-29 23:30:00 +0530

    ADDED WHOLE WEBSITE

---
+++ script.js
@@ -0,0 +1,78 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconAnimation(){
+    var videocon =  document.querySelector("#video-container")
+var playbtn = document.querySelector("#play")
+videocon.addEventListener("mouseenter",function(){
+    gsap.to(playbtn,{
+        scale:1,
+        opacity:1
+    })
+
+})
+videocon.addEventListener("mouseleave",function(){
+    gsap.to(playbtn,{
+        scale:0,
+        opacity:0
+    })
+})
+videocon.addEventListener("mousemove",function(dets){
+    gsap.to(playbtn,{
+        left:dets.x-40,
+        top:dets.y-50
+    })
+})
+
+}
+videoconAnimation()
+
+function loadingAnimation(){
+    gsap.from("#page1 h1",{
+        y:110,
+        opacity:0,
+        delay:0.5,
+        duration:0.9,
+        stagger:0.3
+    })
+    gsap.from("#page1 #video-container",{
+        scale:0.8,
+        opacity:0,
+        delay:1.2,
+        duration:0.5,
+    })
+}
+loadingAnimation()
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})  
+
+//document.querySelector(".child").addEventListener("mouseenter",function(){
+   // gsap.to("#cursor",{
+        //transform: 'translate(-50%,-50%) scale(1)'
+
+    //})
+//})
+
+///document.querySelector(".child").addEventListener("mouseleave",function(){
+   //    transform: 'translate(-50%,-50%) scale(0)'
+
+    //})
+//})
+document.querySelectorAll(".child").forEach(function (elem){
+    elem.addEventListener("mouseenter", function (){
+        gsap.to("#cursor", {
+            transform: "translate(-50%,-50%) scale(1)",
+        });
+    });
+    elem.addEventListener("mouseleave", function (){
+        gsap.to("#cursor",{
+            transform: 'translate(-50%,-50%) scale(0)'
+       });
+   });
+});
